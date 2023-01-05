593bd3be80472d29003f999bc7d2f16192783ea1
SandipIsNotAvailable <2004002.stu@dmscollege.ac.in>
2023-01-05 15:09:22 +0530

    Causes changes

--- src/component/Customer/Custcause.js
+++ src/component/Customer/Custcause.js
@@ -11,52 +11,25 @@ const Custcause = () => {
                 <div className="card-body p-md-5" >
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Your Info</p>
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Causes</p>
+                      <p className="text-center h6 fw-bold mb-5 mx-1 mx-md-4 mt-4">Please select the stage of funds needed.</p>
                       <form className="mx-1 mx-md-4">
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-user fa-lg me-3 fa-fw" />
-                          <div className="form-outline flex-fill mb-0">
-                            <input type="text" id="name" className="form-control" placeholder="Name" />
-                          </div>
+                      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="button" style = {{backgroundColor: 'red'}} className="btn btn-primary btn-md" value="stage">Stage1</button>
                         </div>
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                          <div className="form-outline flex-fill mb-0">
-                            <input type="password" id="form3Example4c" className="form-control" placeholder="Email" />
-                          </div>
-                          </div>
-                          <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                          <div className="form-outline flex-fill mb-0">
-                            <input type="number" id="form3Example7c" className="form-control" placeholder="Contact Number" />
-                          </div>
-                          </div>
-                          <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                          <div className="form-outline flex-fill mb-0">
-                            Cause: <br></br>
-                           <select>
-                                <option value="A">Education</option><br></br>
-                                <option value="B">Medical</option><br></br>
-                           </select>
-                          </div>
-                          </div>
-                          <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                          <div className="form-outline flex-fill mb-0">
-                            Whom are you registring ? <br></br>
-                            <input type="radio" id="form3Example6c" value="Myself" name="who" />Myself <br></br>
-                            <input type="radio" id="form3Example6ca" value="Myself" name="who" />For Others
-                          </div>
-                          </div>
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="button" style = {{backgroundColor: 'orange'}} className="btn btn-primary btn-md" value="stage">Stage2</button>
+                        </div>
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="button" style = {{backgroundColor: 'green'}} className="btn btn-primary btn-md" value="stage">Stage3</button>
+                        </div>
+                        
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                           <button type="button" className="btn btn-primary btn-lg" value="SEND">Submit</button>
                         </div>
                       </form>
                     </div>
-                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image" />
-                    </div>
+                    
                   </div>
                 </div>
               </div>
