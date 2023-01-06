32c452e08bc5bb08ec59b8303e3d04bc1d559443
Omkar-Morajkar <2004012.stu@dmccollege.ac.in>
2023-01-06 21:55:35 +0530

    Created 2nd Login and Register page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Register from "./component/Customer/Register";
 import Login from "./component/Customer/Login";
 import Ahome from "./component/Admin/Ahome";
 import Home from "./component/Customer/Home";
+import Contact from "./component/Customer/Contact";
+import DLogin from "./component/Customer/DLogin";
+import DRegister from "./component/Customer/DRegister";
 
 function App() {
   return(
@@ -18,6 +21,9 @@ function App() {
             <Route path="/Register" element={<Register/>} />
             <Route path="/Login" element={<Login/>} />
             <Route path="/Ahome" element={<Ahome/>}/>
+            <Route path="/Contact" element={<Contact/>}/>
+            <Route path="/DLogin" element={<DLogin/>}/>
+            <Route path="DRegister" element={<DRegister/>}/>
         </Routes>
     </BrowserRouter>
 );
