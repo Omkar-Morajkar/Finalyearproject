fd30a2bfb1f2f80c4c4b695a78b235f109b73af0
sahilkhattal09 <sahilkhattal7@gmail.com>
2023-01-14 21:14:25 +0530

    donate

--- src/component/Customer/CustApi.js
+++ src/component/Customer/CustApi.js
@@ -6,7 +6,7 @@ const Menu = [{
     image:"https://th.bing.com/th/id/OIP.b7u999781VMp4B_4JbPGWgHaE7?w=236&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     name: "meggi",
     description:"My name is pavan de souza",
-    amount:"",
+    amount:"50000",
 },
 {
     id:2,
