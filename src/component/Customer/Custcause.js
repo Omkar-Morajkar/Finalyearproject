bcf4dfdf6bb5f2f68af8ba35611ee6ce3b7c875e
SandipIsNotAvailable <2004002.stu@dmscollege.ac.in>
2023-01-14 07:55:44 +0530

    Just some updates

--- src/component/Customer/Custcause.js
+++ src/component/Customer/Custcause.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Custcause = () => {
     return (
        <>
@@ -15,17 +16,19 @@ const Custcause = () => {
                       <p className="text-center h6 fw-bold mb-5 mx-1 mx-md-4 mt-4">Please select the stage of funds needed.</p>
                       <form className="mx-1 mx-md-4">
                       <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="button" style = {{backgroundColor: 'red'}} className="btn btn-primary btn-md" value="stage">Stage1</button>
+                          <button type="button" style = {{backgroundColor: 'red', width: '250px'}} className="btn btn-primary btn-md" value="stage">Stage1</button>
                         </div>
+                        <p>Select this stage if you are in sever needs of funds <p>e.g within a week.</p></p>
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="button" style = {{backgroundColor: 'orange'}} className="btn btn-primary btn-md" value="stage">Stage2</button>
+                          <button type="button" style = {{backgroundColor: 'orange', width: '250px'}} className="btn btn-primary btn-md" value="stage">Stage2</button>
                         </div>
+                        <p>Select this stage if you need of funds within medium time <p>e.g within a month.</p></p>
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="button" style = {{backgroundColor: 'green'}} className="btn btn-primary btn-md" value="stage">Stage3</button>
+                          <button type="button" style = {{backgroundColor: 'green', width: '250px'}} className="btn btn-primary btn-md" value="stage">Stage3</button>
                         </div>
-                        
+                        <p>Select this stage if you need of funds within moderate time <p>e.g within a year.</p></p>
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button type="button" className="btn btn-primary btn-lg" value="SEND">Submit</button>
+                          <button type="button" className="btn btn-primary btn-lg" value="SEND">Next</button>
                         </div>
                       </form>
                     </div>
