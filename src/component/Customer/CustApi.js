ada222ba9fa3dbb1be5eb9a9f564ae7155b90cf5
sahilkhattal09 <sahilkhattal7@gmail.com>
2023-01-14 20:59:27 +0530

    img

--- src/component/Customer/CustApi.js
+++ src/component/Customer/CustApi.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Menu = [{
     id:1,
     title:"My Little Boy’s Cancer Has Relapsed Twice But I’m Helpless. Please Save Him",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTExtoLVhMIfPRj_8d5RQKF2qjwUbuYL2tZTg&usqp=CAU",
+    image:"https://th.bing.com/th/id/OIP.b7u999781VMp4B_4JbPGWgHaE7?w=236&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     name: "meggi",
     description:"My name is pavan de souza",
     amount:"",
