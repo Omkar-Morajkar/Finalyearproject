519cfa3bb17085574fea696a4e68d6a5b6dad391
sahilkhattal09 <sahilkhattal7@gmail.com>
2023-02-14 13:23:06 +0530

    h

--- src/component/Customer/DonateInput.js
+++ src/component/Customer/DonateInput.js
@@ -14,7 +14,7 @@ function DonateInput(){
                             <div className="card-body p-md-5" >
                                 <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                                   <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign In</p>
-                                  <form className="mx-1 mx-md-4" onSubmit={e => submitForm(e)}>
+                                  <form className="mx-1 mx-md-4">
                                     <div className="d-flex flex-row align-items-center mb-4">
                                       <i className="fas fa-user fa-lg me-3 fa-fw" />
                                       <div className="form-outline flex-fill mb-0">
