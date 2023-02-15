ac17859d82cf5b6e1a5ebe625c47b15ebabd18b6
sahilkhattal09 <sahilkhattal7@gmail.com>
2023-02-15 18:44:47 +0530

    image changed

--- src/component/Customer/DRegister.js
+++ src/component/Customer/DRegister.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Dregister.css';
 
 function DRegister(){
     return(
@@ -67,7 +68,7 @@ function DRegister(){
                       </form>
                     </div>
                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image" />
+                      <img src="https://www.sec.gov/files/crowdfunding-v5b-2016.jpg" className="img-fluid" alt="Sample image" />
                     </div>
                   </div>
                 </div>
