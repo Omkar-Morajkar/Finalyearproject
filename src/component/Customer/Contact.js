c15393800c2fdcb0749934364abd79224ad55125
sahilkhattal09 <sahilkhattal7@gmail.com>
2023-03-02 21:26:53 +0530

    contact

--- src/component/Customer/Contact.js
+++ src/component/Customer/Contact.js
@@ -12,28 +12,26 @@ const Contact = () =>{
         <div className="address details">
           <i className="fas fa-map-marker-alt" />
           <div className="topic">Address</div>
-          <div className="text-one">Surkhet, NP12</div>
-          <div className="text-two">Birendranagar 06</div>
+          <div className="text-one">Mapusa, Boshan building</div>
+          <div className="text-two">floor-3,Mapusa-Goa</div>
         </div>
         <div className="phone details">
           <i className="fas fa-phone-alt" />
           <div className="topic">Phone</div>
-          <div className="text-one">+0098 9893 5647</div>
-          <div className="text-two">+0096 3434 5678</div>
+          <div className="text-one">+0832-2472981</div>
+          <div className="text-two">+0832-2472421</div>
         </div>
         <div className="email details">
           <i className="fas fa-envelope" />
-          <div className="topic">Email</div>
-          <div className="text-one">codinglab@gmail.com</div>
-          <div className="text-two">info.codinglab@gmail.com</div>
+          <div className="topic">Email </div>
+          <div className="text-one">Fund_peti29@gmail.com</div>
+          <div className="text-two">ContactFund_Peti96@gmail.com</div>
         </div>
       </div>
       <div className="right-side">
         <div className="topic-text">Send us a message</div>
         <p>
-          If you have any work from me or any types of quries related to my
-          tutorial, you can send me message from here. It's my pleasure to help
-          you.
+          If you have any sort of queries related to account,terms and conditions or any sort of queries do contact us without hesitation on the provided email,or you can drop a message below
         </p>
         <form action="#">
           <div className="inputbox">
@@ -43,6 +41,11 @@ const Contact = () =>{
             
             <input type="text" placeholder="Enter your email" />
           </div>
+
+         <textarea id="msgtext" class="textmsg"rows={4}  cols={50} placeholder="enter your message here"/>
+
+
+
           <div className="inputbox message-box"></div>
           <div className="button">
             <input type="button" defaultValue="Send Now" />
