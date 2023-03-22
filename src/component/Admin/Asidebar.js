2270f897701521e40a48cb7801a138fc1793cf8e
SandipIsNotAvailable <2004002.stu@dmscollege.ac.in>
2023-03-22 09:10:40 +0530

    Admin sidebar

---
+++ src/component/Admin/Asidebar.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/im";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "./SidebarData";
+import SubMenu from "./SubMenu";
+import { IconContext } from "react-icons/lib";
+import { useNavigate } from 'react-router-dom';
+ 
+const Nav = styled.div`
+background-color: #007bff;
+  border: 3px solid white;
+  width:100%;
+  height: 60px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  box-shadow: inset 0px 10px 10px 0px rgba(0, 0, 0, 0.5);
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+
+`;
+ 
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+//  #87CEFA
+const SidebarNav = styled.nav`
+  background:#ddf2ff ;
+  box-shadow: inset 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+  width:225px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+  transition: 400ms;
+  z-index: 10;
+  
+`
+
+ 
+const SidebarWrap = styled.div`
+  width: 225px;
+`;
+
+ 
+const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const navigate = useNavigate();
+ 
+  const showSidebar = () => setSidebar(!sidebar);
+  if(sidebar && document.getElementById("Login") && document.getElementById("mydiv"))
+  {
+    document.getElementById("Login").style.marginLeft = "225px";
+    document.getElementById("Login").style.transition = '400ms';
+    document.getElementById("mydiv").style.marginLeft = "225px";
+    document.getElementById("mydiv").style.transition = '400ms';
+  }
+  else if(!sidebar && document.getElementById("Login") && document.getElementById("mydiv"))
+  {
+    document.getElementById("Login").style.marginLeft = "0px";
+    document.getElementById("mydiv").style.marginLeft = "0px";
+  }
+
+  const logout = () => {
+    // Reset the user authentication state
+   
+    localStorage.removeItem('login'); // remove the token from localStorage or sessionStorage
+    localStorage.removeItem('Dlogin');
+    localStorage.removeItem('userId');
+    alert("Logout successful")
+    navigate('/Login');
+    
+  };
+  return (
+    <>
+    
+      <IconContext.Provider value={{ color: "black" }}>
+        <div id="mydiv" className="mydiv">
+        <Nav>
+          <NavIcon to="#">
+            <FaIcons.ImMenu onClick={showSidebar}/>
+          </NavIcon>
+          <h2
+            style={{ textAlign: "center",
+                     marginLeft: "200px",
+                     color: "white" }}
+          >
+            Fundpeti
+          </h2>
+          
+            <button type="submit" onClick={logout} className="btn btn-primary btn-lg" name="submit"  style={{ background:'white',color:'blue', position: "absolute", right: '50px', width: '100px',height:'40px',fontSize:'12pt' }}>Logout</button>
+          
+        </Nav>
+        </div>
+        <SidebarNav sidebar={sidebar}>
+          <SidebarWrap>
+            <NavIcon to="#">
+              <AiIcons.AiOutlineClose onClick={showSidebar} />
+            </NavIcon>
+            {SidebarData.map((item, index) => {
+              return <SubMenu item={item} key={index} />;
+            })}
+          </SidebarWrap>
+        </SidebarNav>
+      </IconContext.Provider>
+    </>
+  );
+};
+ 
+export default Sidebar;
